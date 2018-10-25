1762d83327bec2043126fdb1335862b2d7b8773b
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-10-25 22:17:02 +0100

    Refactored

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 # first we print the list of students
+student_count = 11
 puts "The students of Villains Academy"
 puts "--------------------------------"
 puts "Dr. Hannibal Lecter"
@@ -13,9 +14,4 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 #finally, we print the total number of students
-print "Overall, we have "
-# it's important that print() doesn't add a new line characters
-print 11
-puts " great students"
-#
-print "line1\nline2\nline3" # has to be in double quotes!
+puts "Overall, we have #{student_count} great students "
