84e615ea7088af03b096b0fbeec1d12294facabd
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-10-25 22:12:41 +0100

    Added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "The students of Villains Academy"
 puts "--------------------------------"
 puts "Dr. Hannibal Lecter"
@@ -11,9 +12,10 @@ puts "Freddy Kruger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+#finally, we print the total number of students
 print "Overall, we have "
+# it's important that print() doesn't add a new line characters
 print 11
 puts " great students"
-
+#
 print "line1\nline2\nline3" # has to be in double quotes!
