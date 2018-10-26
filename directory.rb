d6e804ba86d9b4e86898ebe0cb82655ca2b86302
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-10-26 14:57:18 +0100

    This version prints students with names less than 12 chars

--- directory.rb
+++ directory.rb
@@ -38,7 +38,7 @@ end
 
 def print(students)
     students.each_with_index do |student, index|
-      if student[:name].chars.first == "N"
+      if student[:name].length < 12
       puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
