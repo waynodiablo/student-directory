856924de72effc1509c841bda66a2eb70c55758a
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-10-26 14:55:33 +0100

    Index added. Also, only returns name beginning with specific character

--- directory.rb
+++ directory.rb
@@ -1,22 +1,22 @@
-# students = [
-# {name: "Dr. Hannibal Lecter", cohort: :november},
-# {name: "Darth Vader", cohort: :november},
-# {name: "Nurse Ratched", cohort: :november},
-# {name: "Michael Corleone", cohort: :november},
-# {name: "Alex DeLarge", cohort: :november},
-# {name: "The Wicked Witch of the West", cohort: :november},
-# {name: "Terminator", cohort: :november},
-# {name: "Freddy Kruger", cohort: :november},
-# {name: "The Joker", cohort: :november},
-# {name: "Joffrey Baratheon", cohort: :november},
-# {name: "Norman Bates", cohort: :november}
-# ]
+students = [
+ {name: "Dr. Hannibal Lecter", cohort: :november},
+ {name: "Darth Vader", cohort: :november},
+ {name: "Nurse Ratched", cohort: :november},
+ {name: "Michael Corleone", cohort: :november},
+ {name: "Alex DeLarge", cohort: :november},
+ {name: "The Wicked Witch of the West", cohort: :november},
+ {name: "Terminator", cohort: :november},
+ {name: "Freddy Kruger", cohort: :november},
+ {name: "The Joker", cohort: :november},
+ {name: "Joffrey Baratheon", cohort: :november},
+ {name: "Norman Bates", cohort: :november}
+ ]
 
-def input_students
+def input_students(students)
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
     # create an empty array
-    students = []
+    #students = []
     # get the first name
     name = gets.chomp
     # while the name is not empty, repeat this code
@@ -37,8 +37,10 @@ def print_header
 end
 
 def print(students)
-    students.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    students.each_with_index do |student, index|
+      if student[:name].chars.first == "N"
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
@@ -46,7 +48,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
-students = input_students
+input_students(students)
 print_header
 print(students)
 print_footer(students)
