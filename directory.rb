190d50df51980fd5b863bafb427058079f767013
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-10-26 16:16:57 +0100

    Added Origins field to the hash and changed input method to take new text

--- directory.rb
+++ directory.rb
@@ -1,28 +1,30 @@
 students = [
- {name: "Dr. Hannibal Lecter", cohort: :november},
- {name: "Darth Vader", cohort: :november},
- {name: "Nurse Ratched", cohort: :november},
- {name: "Michael Corleone", cohort: :november},
- {name: "Alex DeLarge", cohort: :november},
- {name: "The Wicked Witch of the West", cohort: :november},
- {name: "Terminator", cohort: :november},
- {name: "Freddy Kruger", cohort: :november},
- {name: "The Joker", cohort: :november},
- {name: "Joffrey Baratheon", cohort: :november},
- {name: "Norman Bates", cohort: :november}
+ {name: "Dr. Hannibal Lecter", cohort: :november, origin: "Lithuania"},
+ {name: "Darth Vader", cohort: :november, origin: "Tattooine"},
+ {name: "Nurse Ratched", cohort: :november, origin: "America"},
+ {name: "Michael Corleone", cohort: :november, origin: "Sicily"},
+ {name: "Alex DeLarge", cohort: :november, origin: "England"},
+ {name: "The Wicked Witch of the West", cohort: :november, origin: "Oz"},
+ {name: "Terminator", cohort: :november, origin: "SkyNet"},
+ {name: "Freddy Kruger", cohort: :november, origin: "America"},
+ {name: "The Joker", cohort: :november, origin: "Gotham"},
+ {name: "Joffrey Baratheon", cohort: :november, origin: "Kings Landing"},
+ {name: "Norman Bates", cohort: :november, origin: "America"}
  ]
 
 def input_students(students)
-    puts "Please enter the names of the students"
+    puts "Please enter the names of the students, and country of origin"
     puts "To finish, just hit return twice"
-    # create an empty array
+    # create an empty array (turned off to allow for pre-populated data)
     #students = []
     # get the first name
     name = gets.chomp
     # while the name is not empty, repeat this code
     while !name.empty? do
-      # add the student hash to the array
-      students << {name: name, cohort: :november}
+      # add the student hash to the array and ask for Origin
+      puts "Origin? "
+      origin = gets.chomp
+      students << {name: name, cohort: :november, origin: origin}
       puts "Now we have #{students.count} students"
       # get another name from the user
       name = gets.chomp
@@ -42,11 +44,12 @@ end
 #    end
 #end
 
-
+# This print method chnaged to a While loop
+# Original each() iteration above
 def print(students)
     counter = 0
     while counter < students.length do
-        puts "#{counter+1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+        puts "#{counter+1}. #{students[counter][:name]} from #{students[counter][:origin]} (#{students[counter][:cohort]} cohort)"
         counter +=1
     end
 end
