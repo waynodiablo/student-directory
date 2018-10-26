e50e9f118986a3fdfdd3859122917f50b0cbe67f
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-10-26 15:18:32 +0100

    Changed each() iteration to a While loop

--- directory.rb
+++ directory.rb
@@ -36,12 +36,19 @@ def print_header
     puts "--------------------------------"
 end
 
+#def print(students)
+#    students.each_with_index do |student, index|
+#        puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+#    end
+#end
+
+
 def print(students)
-    students.each_with_index do |student, index|
-      if student[:name].length < 12
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    counter = 0
+    while counter < students.length do
+        puts "#{counter+1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+        counter +=1
     end
-  end
 end
 
 def print_footer(students)
